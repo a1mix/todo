6d70543f556715813c7671c5bdfb5dc478af19df
a1mix <Aimix444@gmail.com>
2022-12-12 20:44:56 +0300

    commit for commit

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,16 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <TodoForm />
+  <TodoList />
 </template>
+
+<script>
+import TodoForm from '@/components/TodoForm.vue';
+import TodoList from '@/components/TodoList.vue';
+export default {
+  components: {TodoForm, TodoList}
+}
+</script>
+
+<style>
+
+</style>
