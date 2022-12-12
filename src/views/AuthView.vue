3fa3d18e002bd12a3503d092f35afed25bb1dcbe
a1mix <Aimix444@gmail.com>
2022-12-12 19:55:11 +0300

    router added but not working

---
+++ src/views/AuthView.vue
@@ -0,0 +1,23 @@
+<template>
+  <form action="" @submit.prevent>
+    <stylized-input type="text" />
+    <stylized-input type="password" name="" id="" />
+    <stylized-button>Войти</stylized-button>
+  </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped> 
+form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 30px;
+}
+</style>
